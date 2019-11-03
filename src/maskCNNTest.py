07684e6fb7109dbf5f777b46a1429ca070717096
Zac McLaughlin <zacmclaughlin5@gmail.com>
2019-11-02 21:49:00 -0500

    im eivl

--- src/maskCNNTest.py
+++ src/maskCNNTest.py
@@ -204,7 +204,7 @@ def main():
 
     torch.save(model.state_dict(), "../output/model.p")
 
-    loaded_model = torch.load("../output/output.p")
+    loaded_model = torch.load("../output/model.p")
 
     loaded_model = load_model_instance_segmentation(2, loaded_model)
 
